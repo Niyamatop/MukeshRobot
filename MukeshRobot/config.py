44e2db80574c19a3faa2a9a52795c17cf4886415
Dark souls <87437573+Niyamatop@users.noreply.github.com>
2024-07-20 17:11:10 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,16 +1,16 @@
 class Config(object):
     LOGGER = True
-    API_ID =None 
-    API_HASH = ""
-    TOKEN = ""  
+    API_ID = "28298365" 
+    API_HASH = "9647ab7fc03a8a93f701a5528b2f206e"
+    TOKEN = "7203229265:AAE28YOfCAzCg2LCs8wBvStIGJthpRIY9LQ"  
     OWNER_ID=None
     
     SUPPORT_CHAT = "" 
-    START_IMG = ""
-    EVENT_LOGS = ()
-    MONGO_DB_URI= ""
+    START_IMG = "https://graph.org/file/9fe22ae18601451a70189.jpg"
+    EVENT_LOGS = (-1002239117016)
+    MONGO_DB_URI= "mongodb+srv://Dark123:Dark123@cluster0.jsapbqx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
    
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://avnadmin:AVNS_gwvtPgMcQ7IX0qwaCG9@freedb-mayrice01.i.aivencloud.com:14358/defaultdb?sslmode=require"  # A sql database url from elephantsql.com
     CASH_API_KEY = (
         ""
     )
